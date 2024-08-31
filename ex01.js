cf3b0ebe315db8cb4f1c3b2a5b64c4ce3595a711
Lauanelandim <landim0lauane@hotmail.com>
2024-08-30 21:31:26 -0300

    Add files via upload

---
+++ ex01.js
@@ -0,0 +1,13 @@
+/*Declare duas variáveis chamadas "idade" e "peso",
+atribua valores a elas e imprima os valores no console*/
+
+var idade=27;
+var peso=45;
+
+
+console.log("Olá, você tem "+idade+" anos e o seu peso é de "+peso+"kg");
+//*Olá, você tem 27 anos e seu peso é de 45 kg
+
+let idade=prompt("Qual a sua idade?/n");
+let peso=prompt("Qual o seu peso?/n");
+
